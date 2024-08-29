59751beb05e6ccfd8458522451a16c2c98eeb429
Chron1um635 <chron1um522@gmail.com>
2024-08-29 23:07:18 +0300

    Setup logic

--- Gradient Nazarov MV/ViewController.swift
+++ Gradient Nazarov MV/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var colorfullView: UIView!
+    
+    @IBAction func redSliderChange() {
+        redValueLabel.text = (round(redSlider.value * 100) / 100).formatted()
+        updateViewColor()
     }
 
+    @IBAction func greenSliderChange() {
+        greenValueLabel.text = (round(greenSlider.value * 100) / 100).formatted()
+        updateViewColor()
+    }
 
+    @IBAction func blueSliderChange() {
+        blueValueLabel.text = (round(blueSlider.value * 100) / 100).formatted()
+        updateViewColor()
+    }
+    
+    private func updateViewColor() {
+        colorfullView.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
 }
 

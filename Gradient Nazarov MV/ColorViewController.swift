9018e972f2935089ce2d5fcc9772c563b14f1bb0
Chron1um635 <chron1um522@gmail.com>
2024-09-11 20:46:32 +0300

    update logic

--- Gradient Nazarov MV/ColorViewController.swift
+++ Gradient Nazarov MV/ColorViewController.swift
@@ -19,13 +19,28 @@ final class ColorViewController: UIViewController {
     
     @IBOutlet var colorfullView: UIView!
     
-    var red: CGFloat!
-    var blue: CGFloat!
-    var green: CGFloat!
+    @IBOutlet var redValueTextField: UITextField!
+    @IBOutlet var greenValueTextField: UITextField!
+    @IBOutlet var blueValueTextField: UITextField!
+    
+    private var red: CGFloat = 0
+    private var blue: CGFloat = 0
+    private var green: CGFloat = 0
+    
+    var mainViewColor: UIColor!
+    
+    weak var delegate: ColorViewControllerDelegate?
     
     override func viewDidLoad() {
         colorfullView.layer.cornerRadius = 10
         
+        mainViewColor.getRed(
+            &red,
+            green: &green,
+            blue: &blue,
+            alpha: nil
+        )
+
         redSlider.value = Float(red)
         blueSlider.value = Float(blue)
         greenSlider.value = Float(green)
@@ -35,6 +50,24 @@ final class ColorViewController: UIViewController {
         redValueLabel.text = string(from: redSlider)
         greenValueLabel.text = string(from: greenSlider)
         blueValueLabel.text = string(from: blueSlider)
+        
+        redValueTextField.delegate = self
+        greenValueTextField.delegate = self
+        blueValueTextField.delegate = self
+        
+        redValueTextField.text = string(from: redSlider)
+        greenValueTextField.text = string(from: greenSlider)
+        blueValueTextField.text = string(from: blueSlider)
+        
+        addToolBar(redValueTextField)
+        addToolBar(greenValueTextField)
+        addToolBar(blueValueTextField)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     
@@ -42,16 +75,28 @@ final class ColorViewController: UIViewController {
         switch sender {
         case redSlider:
             redValueLabel.text = string(from: redSlider)
-            red = CGFloat(redSlider.value)
+            redValueTextField.text = string(from: redSlider)
         case greenSlider:
             greenValueLabel.text = string(from: greenSlider)
-            green = CGFloat(greenSlider.value)
+            greenValueTextField.text = string(from: greenSlider)
         default:
             blueValueLabel.text = string(from: blueSlider)
-            blue = CGFloat(blueSlider.value)
+            greenValueTextField.text = string(from: greenSlider)
         }
         
         updateViewColor()
+    }
+    
+    @IBAction func doneButtonPressed() {
+        delegate?.updateViewColor(
+            UIColor(
+                red: CGFloat(redSlider.value),
+                green: CGFloat(greenSlider.value),
+                blue: CGFloat(blueSlider.value),
+                alpha: 1
+            )
+        )
+        dismiss(animated: true)
     }
     
     
@@ -68,5 +113,79 @@ final class ColorViewController: UIViewController {
         String(format: "%.2f", slider.value)
     }
     
+    private func showAlert(with title: String, andMessage message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
 }
 
+// MARK: - UITextFieldDelegate
+extension ColorViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let value = Float(
+            textField.text?.replacingOccurrences(
+                of: ",",
+                with: "."
+            ) ?? ""
+        ) ?? 0
+        if value <= 1 {
+            switch textField {
+            case redValueTextField:
+                redSlider.setValue(value, animated: true)
+                redValueLabel.text = String(value)
+                updateViewColor()
+            case greenValueTextField:
+                greenSlider.setValue(value, animated: true)
+                greenValueLabel.text = String(value)
+                updateViewColor()
+            default:
+                blueSlider.setValue(value, animated: true)
+                blueValueLabel.text = String(value)
+                updateViewColor()
+            }
+        } else {
+            showAlert(
+                with: "Введено не корректное значение",
+                andMessage: "Введите значение в диапазоне 0.0...1.0"
+            )
+        }
+    }
+    
+}
+
+// MARK: - Add tool bar to text field
+extension ColorViewController {
+    func addToolBar(_ textField: UITextField) {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        
+        let spaceArea = UIBarButtonItem(systemItem: .flexibleSpace)
+        let doneButton = UIBarButtonItem(
+            title: "Done",
+            style: .done,
+            target: self,
+            action: #selector(donePressed)
+        )
+        
+        toolBar.setItems([spaceArea, doneButton], animated: false)
+        
+        toolBar.isUserInteractionEnabled = true
+        toolBar.sizeToFit()
+        
+        textField.inputAccessoryView = toolBar
+    }
+    
+    @objc func donePressed(){
+        view.endEditing(true)
+    }
+}
--- Gradient Nazarov MV/MainViewController.swift
+++ Gradient Nazarov MV/MainViewController.swift
@@ -7,40 +7,27 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-    
-    var red: CGFloat = 0
-    var blue: CGFloat = 0
-    var green: CGFloat = 0
+protocol ColorViewControllerDelegate: AnyObject {
+    func updateViewColor(_ color: UIColor)
+}
 
+final class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor?.getRed(
-            &red,
-            green: &green,
-            blue: &blue,
-            alpha: nil
-        )
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let colorVC = segue.destination as? ColorViewController
-        colorVC?.red = red
-        colorVC?.blue = blue
-        colorVC?.green = green
+        colorVC?.delegate = self
+        colorVC?.mainViewColor = view.backgroundColor
     }
     
-    @IBAction func unwind(for segue: UIStoryboardSegue) {
-        let colorVC = segue.source as? ColorViewController
-        red = colorVC?.red ?? 0
-        blue = colorVC?.blue ?? 0
-        green = colorVC?.green ?? 0
-        
-        view.backgroundColor = UIColor(
-            red: red,
-            green: green,
-            blue: blue,
-            alpha: 1
-        )
+}
+
+// MARK: - ColorViewControllerDelegate
+extension MainViewController: ColorViewControllerDelegate {
+    func updateViewColor(_ color: UIColor) {
+        view.backgroundColor = color
     }
 }
